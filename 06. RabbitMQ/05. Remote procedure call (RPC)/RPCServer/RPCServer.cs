4c34d92b0ed86d45f0d720bfcef003e853bb5d38
Zdravko Zdravkov <zdr0@yahoo.com>
2021-10-12 17:41:58 +0200

    05. Remote procedure call (RPC)

---
+++ 06. RabbitMQ/05. Remote procedure call (RPC)/RPCServer/RPCServer.cs
@@ -0,0 +1,126 @@
+﻿namespace RPCServer
+{
+    using System;
+    using System.Text;
+    using RabbitMQ.Client;
+    using RabbitMQ.Client.Events;
+
+    public static class RPCServer
+    {
+        public static void Main(string[] args)
+        {
+            // Factory creates connections to a RabbitMQ node on the local machine - hence the localhost.
+            // If we wanted to connect to a node on a different machine we'd
+            // simply specify its hostname or IP address here.
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            //The connection abstracts the socket connection,
+            //and takes care of protocol version negotiation
+            //and authentication and so on for us.
+            using var connection = factory.CreateConnection();
+
+            // Next we create a channel, which is where most of the API for getting things done resides.
+            using var channel = connection.CreateModel();
+
+            // To send, we must declare a queue for us to send to; then we can publish a message to the queue:
+            // Declaring a queue is idempotent - it will only be created if it doesn't exist already.
+            // Because we might start the consumer before the publisher, we want to make sure the queue
+            // exists before we try to consume messages from it.
+            channel.QueueDeclare(
+                queue: "rpc_queue",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            // This tells RabbitMQ not to give more than one message to a worker at a time.
+            // Or, in other words, don't dispatch a new message to a worker until it
+            // has processed and acknowledged the previous one. Instead, it will dispatch
+            // it to the next worker that is not still busy.
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, false);
+
+            // We're about to tell the server to deliver us the messages from the queue.
+            var consumer = new EventingBasicConsumer(channel);
+
+            
+            // Tell the server to deliver us the messages from the queue.
+            // Manual message acknowledgments are turned on by default. (autoAck: false)
+            channel.BasicConsume(
+                queue: "rpc_queue",
+                autoAck: false,
+                consumer: consumer);
+
+            Console.WriteLine(" [x] Awaiting RPC requests");
+
+            // Since it will push us messages asynchronously, we provide a callback.
+            // That is what EventingBasicConsumer.Received event handler does.
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+
+                // Coping CorrelationId from incoming message to outgoing
+                var props = ea.BasicProperties;
+                var replyProps = consumer.Model.CreateBasicProperties();
+                replyProps.CorrelationId = props.CorrelationId;
+
+                var response = string.Empty;
+
+                try
+                {
+                    // Incoming message
+                    var message = Encoding.UTF8.GetString(body);
+                    var n = int.Parse(message);
+
+                    Console.WriteLine(" [.] fib({0})", message);
+                    response = fib(n).ToString();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(" [.] " + e.Message);
+                    response = "";
+                }
+                finally
+                {
+                    var responseBytes = Encoding.UTF8.GetBytes(response);
+
+                    // Publish messages to our default exchange. 
+                    // We need to supply a routingKey when sending.
+                    consumer.Model.BasicPublish(
+                        exchange: "", 
+                        routingKey: props.ReplyTo,
+                        basicProperties: replyProps, 
+                        body: responseBytes);
+
+                    // It is possible to access the channel via Model property of consumer.
+                    // Using this code we can be sure that even if you kill a worker using
+                    // CTRL+C while it was processing a message, nothing will be lost.
+                    // Soon after the worker dies all unacknowledged messages will be redelivered.
+                    // Acknowledgement must be sent on the same channel that received the delivery.
+                    // Attempts to acknowledge using a different channel will result in a channel-level protocol exception.
+                    consumer.Model.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                }
+            };
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static long fib(int n)
+        {
+            long f1 = 1;
+            long f2 = 1;
+            long f_result = 0;
+
+            long my_counter;
+
+            for (my_counter=2; my_counter < n; my_counter++)
+            {
+                f_result = f1 + f2;
+                f1 = f2;
+                f2 = f_result;
+            }
+
+            return f_result;
+        }
+    }
+}
---
+++ 06. RabbitMQ/05. Remote procedure call (RPC)/RpcClient/Program.cs
@@ -0,0 +1,120 @@
+﻿namespace RpcClient
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using RabbitMQ.Client;
+    using RabbitMQ.Client.Events;
+
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("RPC Client");
+
+            var n = args.Length > 0 ? args[0] : "30";
+            await InvokeAsync(n);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static async Task InvokeAsync(string n)
+        {
+            var rpcClient = new RpcClient();
+
+            Console.WriteLine(" [x] Requesting fib({0})", n);
+            var response = await rpcClient.CallAsync(n);
+            Console.WriteLine(" [.] Got '{0}'", response);
+
+            rpcClient.Close();
+        }
+    }
+
+    public class RpcClient : IDisposable
+    {
+        private const string QUEUE_NAME = "rpc_queue";
+
+        private readonly IConnection connection;
+        private readonly IModel channel;
+        private readonly string replyQueueName;
+
+        private readonly ConcurrentDictionary<string, TaskCompletionSource<string>> callbackMapper;
+
+        public RpcClient()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            this.connection = factory.CreateConnection();
+            this.channel = this.connection.CreateModel();
+            this.replyQueueName = this.channel.QueueDeclare().QueueName;
+
+            this.callbackMapper = new ConcurrentDictionary<string, TaskCompletionSource<string>>();
+
+            this.ConfigureConsumer();
+        }
+
+        public Task<string> CallAsync(string message, CancellationToken cancellationToken = default)
+        {
+            var props = this.channel.CreateBasicProperties();
+
+            var correlationId = Guid.NewGuid().ToString();
+            props.CorrelationId = correlationId;
+            props.ReplyTo = this.replyQueueName;
+
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+
+            var tcs = new TaskCompletionSource<string>();
+            this.callbackMapper.TryAdd(correlationId, tcs);
+
+            this.channel.BasicPublish(
+                exchange: "",
+                routingKey: QUEUE_NAME,
+                basicProperties: props,
+                body: messageBytes);
+
+            cancellationToken.Register(() => this.callbackMapper.TryRemove(correlationId, out var tmp));
+            return tcs.Task;
+        }
+
+        public void Close()
+        {
+            this.channel.Close();
+            this.connection.Close();
+        }
+
+        public void Dispose()
+        {
+            this.channel.Dispose();
+            this.connection.Dispose();
+        }
+
+        private void ConfigureConsumer()
+        {
+            var consumer = new EventingBasicConsumer(this.channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                if (!this.callbackMapper.TryRemove(
+                    ea.BasicProperties.CorrelationId, 
+                    out var tcs))
+                {
+                    return;
+                }
+
+                var body = ea.Body.ToArray();
+                var response = Encoding.UTF8.GetString(body);
+
+                tcs.TrySetResult(response);
+            };
+
+            this.channel.BasicConsume(
+                consumer: consumer,
+                queue: this.replyQueueName,
+                autoAck: true);
+        }
+    }
+}
