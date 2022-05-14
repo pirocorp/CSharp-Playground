f5884f37bd3f82bad35b39dec577638631ee4991
Zdravko Zdravkov <pirocorp@abv.bg>
2022-05-14 22:21:07 +0200

    09. Static abstract members in interfaces

---
+++ 09. Static abstract members in interfaces/Demo/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Demo
+{
+    using System;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            var str = new RepeatSequence();
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine(str++);
+            }
+        }
+
+        public static double MidPoint(double left, double right)
+            => (left + right) / (2.0);
+
+        public static T MidPoint<T>(T left, T right) where T : INumber<T>
+            => left / (T.One + T.One) + right / (T.One + T.One);
+
+        // The constraint that the type argument, T implements IGetNext<T>
+        // ensures that the signature for the operator includes the containing type,
+        // or its type argument. **Many operators enforce that its parameters
+        // must match the type**, or be the type parameter constrained to implement
+        // the containing type. Without this constraint, the ++ operator
+        // couldn't be defined in the IGetNext<T> interface.
+        public interface IGetNext<T> where T : IGetNext<T>
+        {
+            static abstract T operator ++(T other);
+        }
+
+        // with operator can work only with record type, structure type and anonymous type
+        // with expression produces a copy of its operand with the specified properties and fields modified
+        public struct RepeatSequence : IGetNext<RepeatSequence>
+        {
+            private const char Character = 'A';
+
+            private string Text = new(Character, 1);
+
+            public RepeatSequence()
+            {
+            }
+
+            public static RepeatSequence operator ++(RepeatSequence other)
+                => other with { Text = other.Text + Character };
+
+            public override string ToString() => this.Text;
+        }
+    }
+}
---
+++ 09. Static abstract members in interfaces/PointDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace PointDemo
+{
+    using System;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            var pt = new Point<int>(3, 4);
+
+            var translate = new Translation<int>(5, 10);
+
+            var final = pt + translate;
+
+            Console.WriteLine(pt);
+            Console.WriteLine(translate);
+            Console.WriteLine(final);
+        }
+    }
+
+    // It's useful to have a property that defines the additive identity (an identity element
+    // (such as 0 in the group of whole numbers under the operation of addition)
+    // that in a given mathematical system leaves unchanged any element to which it is added) value for the type.
+    // There's a new experimental interface for that feature: IAdditiveIdentity<TSelf, TResult>.
+    // A translation of {0, 0} is the additive identity:
+    public record Translation<T>(T XOffset, T YOffset) : IAdditiveIdentity<Translation<T>, Translation<T>>
+        where T : IAdditiveIdentity<T, T>
+    {
+        public static Translation<T> AdditiveIdentity => new(XOffset: T.AdditiveIdentity, YOffset: T.AdditiveIdentity);
+    }
+    
+    // IAdditionOperators<Point<T>, Translation<T>, Point<T>> interface. The Point type makes use of different types for operands and the result.
+    public record Point<T>(T X, T Y) : IAdditionOperators<Point<T>, Translation<T>, Point<T>>
+        where T : IAdditionOperators<T, T, T>, IAdditiveIdentity<T, T> // Declare that T supports the IAdditionOperators<TSelf, TOther, TResult> interface. That interface includes the operator + static method.
+    {
+        public static Point<T> operator +(Point<T> left, Translation<T> right) 
+            => new(X: left.X + right.XOffset, Y: left.Y + right.YOffset);
+    }
+}
